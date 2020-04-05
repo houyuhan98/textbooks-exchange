31b209ad04462a58dd1c810ee1f76e44675f5fd2
Yuhan Hou <yhou@ufl.edu>
2020-04-05 16:48:15 -0400

    build deploy

---
+++ client/build/precache-manifest.686fa02edc6845b1a05ef70b96b12dab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7232074cea37793dd534f03ab4e111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb5a7adeed70d0f62bfe",
+    "url": "/static/css/2.8929ba51.chunk.css"
+  },
+  {
+    "revision": "2147c3363e79db01f6c6",
+    "url": "/static/css/main.2b264433.chunk.css"
+  },
+  {
+    "revision": "bb5a7adeed70d0f62bfe",
+    "url": "/static/js/2.beff814c.chunk.js"
+  },
+  {
+    "revision": "11c4a93055a6823da6a32bf89d377ea0",
+    "url": "/static/js/2.beff814c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2147c3363e79db01f6c6",
+    "url": "/static/js/main.3592ecf2.chunk.js"
+  },
+  {
+    "revision": "bb9bfb719b1c29f56296",
+    "url": "/static/js/runtime-main.81eef6ac.js"
+  }
+]);
