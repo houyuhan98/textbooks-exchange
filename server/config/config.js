31a2db3f3d6fbf471ed5ea730a3703431be2137a
Yuhan Hou <yhou@ufl.edu>
2020-04-05 21:05:17 -0400

    update

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   db: {
     uri: 'mongodb+srv://houyuhan:Blessyuhan98@bootcamp2-fall2019-wmc76.mongodb.net/TBEC?retryWrites=true&w=majority', //place the URI of your mongo database here.
-  }
+  },
+  sgmail: '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'
 };
