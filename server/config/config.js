5dd821165a690e5d998c6204f0d663268e63e567
Yuhan Hou <yhou@ufl.edu>
2020-04-05 21:13:52 -0400

    update

--- server/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://houyuhan:Blessyuhan98@bootcamp2-fall2019-wmc76.mongodb.net/TBEC?retryWrites=true&w=majority', //place the URI of your mongo database here.
-  }
-};
